16d7a370a41647ffda3d72fc9ead756e5e739567
mitake <mitake.ck@gmail.com>
2015-02-11 18:43:31 +0900

    画面にスプライト追加

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -28,6 +28,17 @@ bool MainScene::init(){
         return false;
     }
     
+    // Director を取り出す
+    auto director = Director::getInstance();
+    // 画面サイズを取り出す
+    auto size = director->getWinSize();
+    // 背景のスプライトを生成する
+    auto background = Sprite::create("background.png");
+    // スプライトの表示位置を設定する
+    background->setPosition(Vec2(size.width/2.0, size.height/2.0));
+    // 親ノードにスプライトを追加する
+    this->addChild(background);
+    
     return true;
 }
 
